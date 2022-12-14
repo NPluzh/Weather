88424a7ff9ffc6fb9b10f114550864a8c48283fd
whittema <shamarsen@mail.ru>
2022-12-14 15:55:07 +0300

    урок 6 | Service + BroadcastReceiver

--- app/src/main/java/com/example/weather/MainActivity.kt
+++ app/src/main/java/com/example/weather/MainActivity.kt
@@ -1,9 +1,18 @@
 package com.example.weather
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.example.weather.another.BUNDLE_KEY
+import com.example.weather.another.MyBroadCastReceiver
+import com.example.weather.another.MyService
 import com.example.weather.another.ThreadsFragment
 import com.example.weather.databinding.ActivityMainBinding
 import com.example.weather.view.weatherlist.WeatherListFragment
@@ -20,6 +29,25 @@ internal class MainActivity : AppCompatActivity() {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, WeatherListFragment.newInstance()).commit()
         }
+        startService(Intent(this, MyService::class.java).apply {
+            putExtra(BUNDLE_KEY,"Hello")
+        })
+
+        val receiver= MyBroadCastReceiver()
+        registerReceiver(receiver, IntentFilter("android.intent.action.AIRPLANE_MODE"))
+        registerReceiver(receiver, IntentFilter("myaction"))
+
+
+
+        LocalBroadcastManager.getInstance(this).registerReceiver(object : BroadcastReceiver(){
+            override fun onReceive(context: Context?, intent: Intent?) {
+                Log.d("@@@"," onReceive ${Thread.currentThread()}")
+            }
+        }, IntentFilter("answer"))
+
+        sendBroadcast(Intent().apply {
+            action = "myaction"
+        })
 
     }
 
---
+++ app/src/main/java/com/example/weather/another/MyBroadCastReceiver.kt
@@ -0,0 +1,12 @@
+package com.example.weather.another
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+
+class MyBroadCastReceiver: BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        Log.d("@@@"," MyBroadCastReceiver ${intent!!.action}")
+    }
+}
