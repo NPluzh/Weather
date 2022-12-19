26eb2a5d68298b0f37c27f6381a12eda7a560d28
whittema <shamarsen@mail.ru>
2022-12-19 15:14:35 +0300

    урок 9 | some fix permission

--- app/src/main/java/com/example/weather/view/contentprovider/ContentProviderFragment.kt
+++ app/src/main/java/com/example/weather/view/contentprovider/ContentProviderFragment.kt
@@ -11,6 +11,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -45,16 +46,53 @@ class ContentProviderFragment : Fragment() {
         checkPermission()
     }
 
-    private fun checkPermission(){
-        val permResult = ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_CONTACTS)
+    private fun checkPermission() {
+        val permResult =
+            ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_CONTACTS)
         PackageManager.PERMISSION_GRANTED
-        if(permResult== PackageManager.PERMISSION_GRANTED){
+        if (permResult == PackageManager.PERMISSION_GRANTED) {
             getContacts()
+        } else if(shouldShowRequestPermissionRationale(Manifest.permission.READ_CONTACTS)){
+            AlertDialog.Builder(requireContext())
+                .setTitle("Доступ к контактам")
+                .setMessage("Объяснение Объяснение Объяснение Объяснение")
+                .setPositiveButton("Предоставить доступ") { _, _ ->
+                    permissionRequest(Manifest.permission.READ_CONTACTS)
+                }
+                .setNegativeButton("Не надо") { dialog, _ -> dialog.dismiss() }
+                .create()
+                .show()
+        } else {
+            permissionRequest(Manifest.permission.READ_CONTACTS)
         }
-        Log.d("@@@","${permResult}")
+
     }
 
-    private fun getContacts(){
+    private fun permissionRequest(permission: String) {
+        requestPermissions(arrayOf(permission), REQUEST_CODE_READ_CONTACTS)
+    }
+
+    private val REQUEST_CODE_READ_CONTACTS = 999
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_CODE_READ_CONTACTS) {
+            for (pIndex in permissions.indices) {
+                if (permissions[pIndex] == Manifest.permission.READ_CONTACTS
+                    && grantResults[pIndex] == PackageManager.PERMISSION_GRANTED
+                ) {
+                    getContacts()
+                    Log.d("@@@", "Ура")
+                }
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    private fun getContacts() {
         val contentResolver: ContentResolver = requireContext().contentResolver
         // Отправляем запрос на получение контактов и получаем ответ в виде Cursor
         val cursorWithContacts: Cursor? = contentResolver.query(
